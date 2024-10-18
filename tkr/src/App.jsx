4fbd0318fe619198f0151444481c5e874cf28d1a
shiva <you@example.com>
2024-10-18 14:48:01 +0530

    first commit

---
+++ tkr/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+  <>
+  <input type="text" placeholder="enter name"/>
+    <input type="email" placeholder="enter mail"/>
+    <input type="date" placeholder="enter age" />
+    <input type="submit" button="submit" />
+    
+  </>
+  )
+}
+
+export default App
